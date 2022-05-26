62e75ca202cce1006188f3702ee44171368b5fda
briankimm <66752469+briankimm@users.noreply.github.com>
2022-05-26 13:30:42 -0400

    Add files via upload

---
+++ lab3/inlab/testPostfixCalc.cpp
@@ -0,0 +1,9 @@
+//Brian Kim, byk6q, 9/13/21, testPostfixCalc.cpp
+#include <iostream>
+#include "postfixCalculator.h"
+using namespace std;
+int main(){
+  postfixCalculator p;
+  p.calc();//calls calc function
+  return 0;
+}
