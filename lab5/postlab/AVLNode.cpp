7ac9e14a86926c9f4364204ee989ecb34b552444
briankimm <66752469+briankimm@users.noreply.github.com>
2022-05-26 13:36:33 -0400

    Create AVLNode.cpp

---
+++ lab5/postlab/AVLNode.cpp
@@ -0,0 +1,18 @@
+//Brian Kim, byk6q, AVLNode.cpp, 10/9/21
+#include "AVLNode.h"
+#include <string>
+using namespace std;
+
+AVLNode::AVLNode() {
+    value = "?";
+    left = NULL;
+    right = NULL;
+    height = 0;
+}
+
+AVLNode::~AVLNode() {
+    delete left;
+    delete right;
+    left = NULL;
+    right = NULL;
+}
