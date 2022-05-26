c1d01dbcd51370c27ab6604c789c3cc23d53ff1e
briankimm <66752469+briankimm@users.noreply.github.com>
2022-05-26 13:29:28 -0400

    Add files via upload

---
+++ lab3/prelab/postfixCalculator.h
@@ -0,0 +1,21 @@
+//Brian Kim, byk6q, 9/13/21, postfixCalculator.h
+#ifndef POSTFIXCALCULATOR_H
+#define POSTFIXCALCULATOR_H
+#include <iostream>
+#include <stack>
+#include <cstdlib>
+using namespace std;
+class postfixCalculator {
+ public:
+  postfixCalculator(); //default constructor
+  ~postfixCalculator(); //destructor
+  void add();//add method
+  void subtract();//subtract method
+  void multi();// multiplication method
+  void div();//division method
+  void negation();//unary negation
+  void calc();
+ private:
+  stack <int> s; // stack to hold the elements
+};
+#endif
