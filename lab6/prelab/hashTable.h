b05358c77c85e561d7aa3454b4e5c4cbaa2cc847
briankimm <66752469+briankimm@users.noreply.github.com>
2022-05-26 13:39:16 -0400

    Add files via upload

---
+++ lab6/prelab/hashTable.h
@@ -0,0 +1,27 @@
+//Brian Kim, byk6q, 10/19/21,hashTable.h
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include "timer.h"
+
+using namespace std;
+
+class hashTable{
+ public:
+  hashTable();//constructor
+  hashTable(int i);//constructor with parameter size
+  ~hashTable();//destructor
+
+  bool contains(string s);//find method
+  void insert(string s);//insert method
+  int hash(string s);//hash method
+
+ private:
+  vector<list<string> > v;
+  
+};
+#endif
